1ec024d3a7daf468cbe0d901355c7dcc014b124e
10 <tochka098@gmail.com>
2022-09-29 20:03:01 +0300

    index.js improvements

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,19 @@
 const tabLinks = document.querySelectorAll(".tabs__item");
 const tabContent = document.querySelectorAll(".content__item");
 
-tabLinks.forEach(function (el) {
-  el.addEventListener("click", openTabs);
+tabLinks.forEach((el, index) => {
+  el.addEventListener("click", function openTabs() {
+    console.log(index);
+
+    tabContent.forEach(function (content) {
+      content.classList.remove("active");
+    });
+
+    tabLinks.forEach(function (link) {
+      link.classList.remove("active");
+    });
+
+    el.classList.add("active");
+    tabContent[index].classList.add("active");
+  });
 });
-
-function openTabs(el) {
-  const btnTarget = el.currentTarget;
-  const company = btnTarget.dataset.company;
-
-  tabContent.forEach(function (el) {
-    el.classList.remove("active");
-  });
-
-  tabLinks.forEach(function (el) {
-    el.classList.remove("active");
-  });
-
-  btnTarget.classList.add("active");
-
-  document.getElementById(company).classList.add("active");
-}
